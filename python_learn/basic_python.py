5a5e16de6c9231eb7dfc9361efac90caefd9a841
Manutama aditya <55338832+kmnvz-mayvez@users.noreply.github.com>
2023-10-05 10:37:05 +0700

    _update

--- python_learn/basic_python.py
+++ python_learn/basic_python.py
@@ -48,6 +48,8 @@ x = memoryview(bytes(5))                      # memoryview
 x = 1    # int
 y = 2.8  # float
 z = 1j   # complex
+j = 1 
+k = (j + z)
 
 #convert from int to float:
 a = float(x)
@@ -61,7 +63,51 @@ c = complex(x)
 print(a)
 print(b)
 print(c)
+print(k)
 
 print(type(a))
 print(type(b))
 print(type(c))
+print(type(k))
+
+# python casting
+
+z = 4.2 
+
+print(type(z))
+print(str(z))
+
+# python strings 
+
+a = "Hello, World!"
+print(len(a))
+
+a = "Hello, World!"
+print(a[7])
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if 'best' in txt:
+   print(txt[4:-25])
+   print(txt.split('best'))
+   print(txt.replace("best","and"))
+
+a = "the"
+b = "best"
+c = a[2] + a[:2] + b[2]
+print(c)
+print(len(c))
+z = float(len(c))
+print(z)
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+
+
+
